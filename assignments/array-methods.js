4fdb2dcb58cfead68d6f14d7d194df3e1306f5e2
Tara Gamache <tgamache.13@gmail.com>
2019-08-21 04:05:11 -0700

    working on stretch goals

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -91,6 +91,8 @@ console.log(ticketPriceTotal);
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+//The event director wants to thank each company for having a runner in the race. We need a list of runners full names and their company.
+
 
 // Problem 2
 
